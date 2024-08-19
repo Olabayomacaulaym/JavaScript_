83f395a7b79f125a3773e1975dfe7f91def49330
Olabayomacaulaym <olabaiyomacaulay@gmail.com>
2024-08-19 00:59:23 +0000

    Update 01.js
    Aprende a respeito do objeto no javascript .

--- JS_Guia_definitivo_/01.js
+++ JS_Guia_definitivo_/01.js
@@ -5,6 +5,12 @@ var book = {
     fat: true  
    
     }
+// Estou criando novas propriedades por meio da atribuição dentro objeto que pertence ao variável "book"
+book.author = 20 ; 
+// Estou criando uma nova variável que vai receber um novo objeto 
+var tapona = { 
 
-book.topic 
-console.log ( book)
+    prada : 29 
+}
+console.log ( book.topic , book["fat"],book.author) ; 
+console.log ( tapona.prada)
